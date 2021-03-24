cf7d9b3170ea78572347e97a1190162635434ad3
Milan Mody <milanm1120@gmail.com>
2021-03-24 01:47:43 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -17,9 +17,9 @@ class Song
     @song = []
   end
 
-  def self.find_by_name(name)
-    @@songs.detect{|a| a.name == name}
-  end
+  # def self.find_by_name(name)
+  #   @@songs.detect{|a| a.name == name}
+  # end
 
   def self.all
     @@songs
