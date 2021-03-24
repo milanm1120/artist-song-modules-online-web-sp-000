98edfebd85baad8585f66ba9836bad13c685ffd9
Milan Mody <milanm1120@gmail.com>
2021-03-24 01:31:39 +0000

    Automatically backed up by Learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,10 +1,17 @@
 module Memorable
-
-  def reset_all
+  module ClassMethods
+    def reset_all
       self.all.clear
     end
 
     def count
       self.all.count
     end
+  end
+
+  module InstanceMethods
+    def initialize
+      self.class.all << self
+    end
+  end
 end
