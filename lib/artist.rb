dd53e996bbf5ffe12e84c290b8dfbbae71d7b518
Milan Mody <milanm1120@gmail.com>
2021-03-24 01:43:03 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -17,16 +17,12 @@ class Artist
     @song = []
   end
 
-  def self.find_by_name(name)
-    @@artists.detect{|a| a.name == name}
-  end
+  # def self.find_by_name(name)
+  #   @@artists.detect{|a| a.name == name}
+  # end
 
   def self.all
     @@artists
-  end
-
-  def self.find_by_name(name)
-    @@artists.detect {|a| a.name == name}
   end
 
   def add_song(song)
